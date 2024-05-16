5f1b0fb8cfa477c97590bc9170050ba169f0bbee
Soham Sangole <sohamajaysangole@gmail.com>
2024-05-17 02:24:04 +0530

    Updated Projects

--- portfolio/src/app/TopText.js
+++ portfolio/src/app/TopText.js
@@ -7,7 +7,7 @@ const TopText = () => {
                 <p className="text-lg text-white">
                     My portfolio covers a wide range of topics, including supervised machine learning, deep learning, reinforcement learning, application development, and web development. I&apos;m continually extending my skill set and project list. The most exciting things I learned include, training machine learning models and hosting them on the web and in apps with Flask. Below are links to my other projects, including code, blog posts, and deployed versions.
                 </p>
-                <h1 className="text-3xl font-bold text-white py-4">PERSONAL PORTFOLIO</h1>
+                <h1 className="text-3xl font-bold text-white py-4">PROJECT PORTFOLIO</h1>
             </div>
         </div>
     )
